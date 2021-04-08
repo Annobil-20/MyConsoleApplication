922633ac1f60caca53acd803056f4c607db07858
Annobil-20 <annobils37@gmail.com>
2021-04-08 11:44:09 +0000

    C++ Program to take user's input
    This program takes the input of a user that is the first name and the last name respectively after which the result is displayed.

---
+++ project1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string first_name, last_name;
+    cout<<"Enter your first name:"<<endl;
+    cin>>first_name;
+    cout << "Enter your last name:" <<endl;
+    cin >> last_name;
+
+    cout<<"Your fullname is " <<first_name+" "+last_name<<endl;
+    return 0;
+}
